10366201dd38c6c288966e4b9bd9840c13f46bb4
Yu Kodama <yuu.kdm@gmail.com>
2015-03-22 23:41:58 +0900

    exe_SVM_lik.pyを追加

---
+++ AR_exp06/exe_SVM_lik.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+import os
+
+for lenSeq_hmm in [5, 20, 50]:
+	for n_states in [1, 2, 3, 4, 5]:
+		for lenSeq_svm in [5, 20, 50]:
+			cmd = "echo 'source .bashrc; cd /home/kodama-y/work/ActionRecognition/AR_exp06/; date; \
+				   python SVM_lik.py %d %d %d; date' | qsub" % (lenSeq_hmm, n_states, lenSeq_svm)
+			os.system(cmd)
