6af34624b96a530e90451e66122112a42aedec29
Yu Kodama <yuu.kdm@gmail.com>
2015-04-07 22:05:47 +0900

    HMMの隠れ状態数とSVM,HMMの入力シーケンス長を変更

--- AR_exp06/exe_SVM_lik.py
+++ AR_exp06/exe_SVM_lik.py
@@ -1,9 +1,9 @@
 #coding: utf-8
 import os
 
-for lenSeq_hmm in [5, 20, 50]:
-	for n_states in [5, 10, 15, 20]:
-		for lenSeq_svm in [5, 20, 50]:
+for lenSeq_hmm in [20, 40, 60]:
+	for n_states in [5, 10, 15]:
+		for lenSeq_svm in [20, 40, 60]:
 			cmd = "echo 'source .bashrc; cd /home/kodama-y/work/ActionRecognition/AR_exp06/; date; \
 				   python SVM_lik.py %d %d %d; date' | qsub" % (lenSeq_hmm, n_states, lenSeq_svm)
 			os.system(cmd)
