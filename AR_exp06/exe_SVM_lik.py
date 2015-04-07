e68a550c0bd65f7884000d8477675c2f90c23538
Yu Kodama <yuu.kdm@gmail.com>
2015-04-07 21:49:15 +0900

    隠れ状態数を変更

--- AR_exp06/exe_SVM_lik.py
+++ AR_exp06/exe_SVM_lik.py
@@ -2,7 +2,7 @@
 import os
 
 for lenSeq_hmm in [5, 20, 50]:
-	for n_states in [1, 2, 3, 4, 5]:
+	for n_states in [5, 10, 15, 20]:
 		for lenSeq_svm in [5, 20, 50]:
 			cmd = "echo 'source .bashrc; cd /home/kodama-y/work/ActionRecognition/AR_exp06/; date; \
 				   python SVM_lik.py %d %d %d; date' | qsub" % (lenSeq_hmm, n_states, lenSeq_svm)
