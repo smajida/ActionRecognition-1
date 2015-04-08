0395d64f8790399cc18480c3414c5aa6752b09e6
Yu Kodama <yuu.kdm@gmail.com>
2015-04-08 17:45:30 +0900

    隠れ状態数を5,10,15に変更

--- AR_exp05/compute_lik/exe_MultHMM_lik.m
+++ AR_exp05/compute_lik/exe_MultHMM_lik.m
@@ -1,4 +1,4 @@
-for s = 2:5
+for s = [5, 10, 15]
 	for K = [50, 100, 150]
 		for T = [10, 30, 50]
 		command = sprintf('qsh matlab -nodesktop -singleCompThread -nojvm -r ''"MultHMM_lik(%d,%d,%d); quit;"''', s,K,T);
